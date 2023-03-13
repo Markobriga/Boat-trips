815c958356344b20308a5768818ec881d60e81df
Markobriga <marko.brigic@gmail.com>
2023-03-13 16:24:02 +0100

    Reset password

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -5,6 +5,8 @@ const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
 const sendToken = require('../utils/jwtToken');
 const sendEmail = require('../utils/sendEmail');
 
+const crypto = require('crypto');
+
 // Register a user => /api/v1/register
 exports.registerUser = catchAsyncErrors( async( req,res,next ) => {
 
@@ -82,6 +84,34 @@ exports.forgotPassword = catchAsyncErrors(async(req,res,next) => {
         return next(new ErrorHandler(error.message, 500))
     }
 
+})
+
+// Reset password => /api/v1/password/reset/:token
+exports.resetPassword = catchAsyncErrors( async (req, res, next) => {
+
+    const resetPasswordToken = crypto.createHash('sha256').update(req.params.token).digest('hex');
+    const user = await User.findOne({
+        resetPasswordToken,
+        resetPasswordExpire: {
+            $gt: Date.now()
+        }
+    })
+
+    if(!user) {
+        return next(new ErrorHandler('Invalid token',400));
+    }
+
+    if(req.body.password !== req.body.confirmPassword) {
+        return next(new ErrorHandler('Password and confirm password do not match', 400));
+    }
+
+   user.password = req.body.password;
+   user.resetPasswordToken = undefined;
+   user.resetPasswordExpire = undefined;
+   
+   await user.save();
+
+   sendToken(user, 200, res)
 })
 
 // Logout a user => /api/v1/logout
