d4a3a98dda8999c7125a250677ab5fe8f547c408
Markobriga <marko.brigic@gmail.com>
2023-03-13 13:18:31 +0100

    Authorize user roles

--- backend/routes/boat.js
+++ backend/routes/boat.js
@@ -3,13 +3,13 @@ const router = express.Router();
 
 const { getBoats, newBoat, getSingleBoat, updateBoat, deleteBoat } = require('../controllers/boatController')
 
-const { isAuthenticatedUser } = require('../middlewares/auth');
+const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
 
 router.route('/boats').get(getBoats);
 router.route('/boat/:id').get(getSingleBoat);
 
-router.route('/admin/boat/new').post(isAuthenticatedUser, newBoat);
+router.route('/admin/boat/new').post(isAuthenticatedUser, authorizeRoles('admin'), newBoat);
 
-router.route('/admin/boat/:id').put(isAuthenticatedUser, updateBoat).delete(isAuthenticatedUser, deleteBoat);
+router.route('/admin/boat/:id').put(isAuthenticatedUser, authorizeRoles('admin'), updateBoat).delete(isAuthenticatedUser, authorizeRoles('admin'), deleteBoat);
 
 module.exports = router
--- backend/routes/trip.js
+++ backend/routes/trip.js
@@ -3,14 +3,14 @@ const router = express.Router();
 
 const {newTrip, getTrips, getSingleTrip, updateTrip, deleteTrip} = require('../controllers/tripController');
 
-const { isAuthenticatedUser } = require('../middlewares/auth');
+const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
 
 router.route('/trips').get(getTrips);
 router.route('/trip/:id').get(getSingleTrip);
 
-router.route('/admin/trip/new').post(isAuthenticatedUser, newTrip);
+router.route('/admin/trip/new').post(isAuthenticatedUser, authorizeRoles('admin'), newTrip);
 
-router.route('/admin/trip/:id').put(isAuthenticatedUser, updateTrip).delete(isAuthenticatedUser, deleteTrip);
+router.route('/admin/trip/:id').put(isAuthenticatedUser, authorizeRoles('admin'), updateTrip).delete(isAuthenticatedUser, authorizeRoles('admin'), deleteTrip);
 
 
 module.exports = router;
