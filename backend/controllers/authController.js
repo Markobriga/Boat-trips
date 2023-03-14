975e1d21873cd8c8f8983f37811005115e2b8eb5
Markobriga <marko.brigic@gmail.com>
2023-03-14 12:54:21 +0100

    Get user profile

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -114,6 +114,17 @@ exports.resetPassword = catchAsyncErrors( async (req, res, next) => {
    sendToken(user, 200, res)
 })
 
+//  Get currently logged in user => api/v1/me
+exports.getUserProfile = catchAsyncErrors( async (req, res, next) => {
+
+    const user = await User.findById(req.user.id);
+
+    res.status(200).json({
+        success: true,
+        user
+    })
+})
+
 // Logout a user => /api/v1/logout
 exports.logout = catchAsyncErrors( async( req,res,next ) => {
 
