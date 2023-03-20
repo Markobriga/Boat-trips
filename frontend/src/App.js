7efbd030bac266df9bd797eed6918841d8706e05
Markobriga <marko.brigic@gmail.com>
2023-03-20 15:47:16 +0100

    Added router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,21 @@
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
 import './App.css';
 import Header from './modules/Header';
 import Footer from './modules/Footer';
+import Home from './pages/Home'
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <div>Boat trips</div>
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" component={Home} exact />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
