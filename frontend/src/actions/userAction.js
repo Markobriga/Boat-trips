7fc48b8c65ad89e637022b2831f36097695769a7
Markobriga <marko.brigic@gmail.com>
2023-03-23 14:26:02 +0100

    Added register user reducer and action

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { LOGIN_REQUEST, LOGIN_FAIL, LOGIN_SUCCESS, CLEAR_ERRORS } from "../constants/userConstants";
+import { LOGIN_REQUEST, LOGIN_FAIL, LOGIN_SUCCESS, REGISTER_USER_REQUEST, REGISTER_USER_SUCCESS, REGISTER_USER_FAIL, CLEAR_ERRORS } from "../constants/userConstants";
 
 
 export const login = (email, password) => async (dispatch) => {
@@ -21,6 +21,31 @@ export const login = (email, password) => async (dispatch) => {
     } catch (error) {
         dispatch({
             type: LOGIN_FAIL,
+            payload: error.response.data.message
+        })
+    }
+}
+
+export const register = (userData) => async (dispatch) => {
+    try {
+        dispatch({ type: REGISTER_USER_REQUEST})
+
+        const config = {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        }
+
+        const { data } = await axios.post('api/v1/register', userData, config)
+
+        dispatch({
+            type: REGISTER_USER_SUCCESS,
+            payload: data.user
+        })
+
+    } catch (error) {
+        dispatch({
+            type: REGISTER_USER_FAIL,
             payload: error.response.data.message
         })
     }
