c2a4dfeb041343fc62eb2731a9cc622a12de8d98
Markobriga <marko.brigic@gmail.com>
2023-03-23 15:55:01 +0100

    Added passwords matching

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -10,8 +10,14 @@ const crypto = require('crypto');
 // Register a user => /api/v1/register
 exports.registerUser = catchAsyncErrors( async( req,res,next ) => {
 
-    const {name, email, password} = req.body;
-    
+    const {name, email, password, confirmPassword} = req.body;
+
+    console.log(req.body.name, req.body.email, req.body.password)
+
+    if(password !== confirmPassword) {
+        return next(new ErrorHandler('Passwords do not match',401))
+    }
+
     const user = await User.create({
         name,
         email,
