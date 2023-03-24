3e5dd9f27e9f8d9a431dd54a125ecabe6e743fda
Markobriga <marko.brigic@gmail.com>
2023-03-24 13:16:25 +0100

    Added dropdown in header

---
+++ frontend/src/components/dropdownHeader.js
@@ -0,0 +1,87 @@
+import { Fragment } from 'react'
+import { Menu, Transition } from '@headlessui/react'
+
+export default function DropdownHeader({user}) {
+
+    function classNames(...classes) {
+        return classes.filter(Boolean).join(' ')
+      }
+
+    return (
+        <Menu as="div" className="relative inline-block text-left">
+          <div>
+            <Menu.Button className='text-white font-medium text-sm mr-4'>
+                Hi, {user && user.name}
+            </Menu.Button>
+          </div>
+    
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+              <div className="py-1">
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="#"
+                      className={classNames(
+                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                        'block px-4 py-2 text-sm'
+                      )}
+                    >
+                      Profile
+                    </a>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="#"
+                      className={classNames(
+                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                        'block px-4 py-2 text-sm'
+                      )}
+                    >
+                      Dashboard
+                    </a>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="#"
+                      className={classNames(
+                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                        'block px-4 py-2 text-sm'
+                      )}
+                    >
+                      License
+                    </a>
+                  )}
+                </Menu.Item>
+                
+                <Menu.Item>
+                    {({ active }) => (
+                        <button
+                        className={classNames(
+                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                            'block w-full px-4 py-2 text-left text-sm'
+                        )}
+                        >
+                        Sign out
+                        </button>
+                        )}
+                </Menu.Item>
+              
+              </div>
+            </Menu.Items>
+          </Transition>
+        </Menu>
+      )
+}
--- frontend/src/modules/Header.js
+++ frontend/src/modules/Header.js
@@ -4,6 +4,7 @@ import { XMarkIcon } from '@heroicons/react/24/outline'
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux'
 import { logout } from '../actions/userAction';
+import DropdownHeader from '../components/dropdownHeader';
 
 
 const Header = () => {
@@ -29,7 +30,7 @@ const Header = () => {
                     <div className="flex items-center lg:order-2">
                         {user ? (
                             <div>
-                                <span className='text-white font-medium text-sm mr-4'>Hi, {user && user.name}</span>
+                                <DropdownHeader user={user} />
                                 <Link to="/" onClick={logoutHandler} className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">Logout</Link>
                             </div>
                         ) : !loading && (
