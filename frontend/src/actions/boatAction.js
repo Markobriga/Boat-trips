00061ccd54d25ea0e08e798fe8043681e9ca680a
Markobriga <marko.brigic@gmail.com>
2023-04-12 13:36:24 +0200

    Added action to add new baot

--- frontend/src/actions/boatAction.js
+++ frontend/src/actions/boatAction.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { ALL_BOATS_REQUEST, ALL_BOATS_SUCCESS, ALL_BOATS_FAIL, BOAT_DETAILS_REQUEST, BOAT_DETAILS_SUCCESS, BOAT_DETAILS_FAIL, NEW_REVIEW_REQUEST, NEW_REVIEW_SUCCESS, NEW_REVIEW_FAIL, NEW_REVIEW_RESET, NEW_BOAT_REQUEST, NEW_BOAT_SUCCESS, NEW_BOAT_FAIL, NEW_BOAT_RESET, CLEAR_ERRORS} from '../constants/boatConstants'
+import { ALL_BOATS_REQUEST, ALL_BOATS_SUCCESS, ALL_BOATS_FAIL, BOAT_DETAILS_REQUEST, BOAT_DETAILS_SUCCESS, BOAT_DETAILS_FAIL, NEW_REVIEW_REQUEST, NEW_REVIEW_SUCCESS, NEW_REVIEW_FAIL, NEW_BOAT_REQUEST, NEW_BOAT_SUCCESS, NEW_BOAT_FAIL, CLEAR_ERRORS} from '../constants/boatConstants'
 
 export const getBoats = () => async (dispatch) => {
     try {
@@ -19,6 +19,34 @@ export const getBoats = () => async (dispatch) => {
         })
     }
 }
+
+export const newBoat = (boatData) => async (dispatch) => {
+    try {
+
+        dispatch({ type: NEW_BOAT_REQUEST })
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        const { data } = await axios.put(`/api/v1/admin/boat/new`, boatData, config)
+
+        dispatch({
+            type: NEW_BOAT_SUCCESS,
+            payload: data
+        })
+
+    } catch (error) {
+
+        dispatch({
+            type: NEW_BOAT_FAIL,
+            payload: error.response.data.message
+        })
+    }
+}
+
 
 export const getBoatDetails = (id) => async (dispatch) => {
     try {
