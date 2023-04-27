248d1e5249882b72b399d72a7341f6dabe0461de
Markobriga <marko.brigic@gmail.com>
2023-04-27 16:39:36 +0200

    Added trips to home page

---
+++ frontend/src/components/tripHomeCard.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { format } from 'date-fns'
+import ReactStars from "react-rating-stars-component";
+
+const TripHomeCard = ({trip}) => {
+
+    return (
+        <div className='mb-5 px-2'>
+            <Link to={`/trip/${trip._id}`} className="flex flex-col items-start bg-white  hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                <img className="object-cover w-full  sm:max-h-48 h-auto  sm:h-auto aspect-video" src={trip.boat.images[0].url} alt="" />
+                <div className="flex flex-col text-start py-1 leading-snug sm:w-full">
+                    <div className=''>{format(new Date(trip.date), 'dd.MM.yyyy')}</div>
+                    <div className=''>
+                        <span className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{trip.tripName}</span>
+                        <span className='text-2xl italic tracking-tight text-gray-900 dark:text-white pl-1.5'>by {trip.boat.name}</span>
+                    </div>
+                    <div className='flex '>
+                        <ReactStars 
+                            count={5}
+                            value={trip.boat.ratings}
+                            edit={false}
+                        />
+                        <div className='text-xs pt-1'>({trip.boat.numOfReviews})</div>
+                    </div>
+                    
+                    <div>{trip.location.toString()}</div>
+                    <div>Price Adult: {trip.priceAdult}€</div>
+                    <div>Price Child: {trip.priceChild}€</div>
+                    <div>Duration: {trip.duration}h</div>
+                </div>
+            </Link>
+        </div>
+    )
+}
+
+export default TripHomeCard
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,27 +1,51 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from 'react-redux'
 import Loader from "../components/Loader";
 import { getPosts } from "../actions/postAction";
-import BlogCard from "../components/blogCard";
 import BlogHomeCard from "../components/blogHomeCard";
+import { getNextTrips } from "../actions/tripAction";
+import TripCard from "../components/tripCard";
+import TripHomeCard from "../components/tripHomeCard";
 
 const Home = () => {
 
     const dispatch = useDispatch()
 
     const { loading, posts} = useSelector(state=>state.posts)
+    const { loading: loadingTrips, trips } = useSelector(state => state.trips)
+    const [ popularIndex, setPopularIndex ] = useState([])
 
     useEffect(()=>{
         dispatch(getPosts())
+        dispatch(getNextTrips([1,200],[1,200],[], new Date(), false))
     },[dispatch])
 
+    useEffect(()=>{
+        if(!loadingTrips && trips) {
+            let temp=trips.map((trip,index)=> ({
+                percentage: trip.numberOfReservations / trip.boat.maxNumberOfReservations,
+                index: index
+            }))
+            findPopularTrips(temp)
+            console.log(temp)
+        }
+    },[trips])
+
+    const findPopularTrips = (temp) => {
+        temp.sort((a, b) => a.percentage - b.percentage)
+        let result = temp.filter(trip => trip.percentage < 1)
+        let indexes = result.map(a => a.index)
+        setPopularIndex(indexes.slice(0,3))
+    }
+
+    console.log(popularIndex)
 
     return (
         <div className="flex flex-col">
             <div className="relative">
                 <img src={require("../images/makarska.jpg")}></img>
                 <div className="absolute  text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-                    <div className="text-3xl sm:text-4xl md:text-6xl lg:text-8xl">
+                    <div className="text-3xl sm:text-4xl md:text-6xl lg:text-8xl font-semibold">
                         Boat Trips
                     </div>
                     <div className="sm:text-lg md:text-xl lg:text-2xl">
@@ -30,9 +54,29 @@ const Home = () => {
                 </div>
             </div>
             <div className="mx-auto max-w-screen-xl flex w-full justify-center py-5">
-                {loading ? <Loader /> :
+                {!loading && !loadingTrips && 
+                    <div>
+                        <div className="text-2xl md:text-5xl font-semibold">
+                            Makarska Riviera boat trips 
+                        </div>
+                        <div className="text-md md:text-2xl">
+                            the most exciting trips
+                        </div>
+                        <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 justify-center">
+                            {trips && trips.map((trip,index) => (
+                                <div>
+                                    {popularIndex.includes(index) && 
+                                    <TripHomeCard trip={trip} key={trip._id} />}
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                }
+            </div>
+            <div className="mx-auto max-w-screen-xl flex w-full justify-center py-5">
+                {(loading || loadingTrips) ? <Loader /> :
                     <div >
-                        <div className="text-2xl md:text-5xl">
+                        <div className="text-2xl md:text-5xl font-semibold">
                             Makarska Riviera boat trips blog 
                         </div>
                         <div className="text-md md:text-2xl">
