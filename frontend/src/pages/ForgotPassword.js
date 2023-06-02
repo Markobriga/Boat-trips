981b7f5abc13af4c13d1a04f418997d1acabc10f
Markobriga <marko.brigic@gmail.com>
2023-06-02 13:12:32 +0200

    Changed background color

--- frontend/src/pages/ForgotPassword.js
+++ frontend/src/pages/ForgotPassword.js
@@ -32,7 +32,7 @@ const ForgotPassword = () => {
     }
 
     return (
-        <section className="bg-gray-50 dark:bg-gray-900">
+        <section className=" dark:bg-gray-900">
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:py-32">
                 <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                     <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
